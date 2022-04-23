cd35d2282aaa9a9eea9b07f2935e6026f637fe3a
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-04-23 19:21:04 +0200

    Add validations to models

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,7 @@
 class Payment < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :categories
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 250 }
+  validates :amount, numericality: { greater_than: 0 }
 end
